5ce9913140c893f93ad72dd070a3e7bf9facd56a
muhamed-hassan <mohamed.hassan.work@gmail.com>
2024-04-03 13:53:23 +0200

    add project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './fragments/Home';
+import Fragment_1 from './fragments/Fragment_1';
+import Fragment_2 from './fragments/Fragment_2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Simple menu demo
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Header />
+
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/fragment_1" element={<Fragment_1 />} />
+                        <Route path="/fragment_2" element={<Fragment_2 />} />
+                    </Routes>
+                </BrowserRouter>
+
+                <Footer />
+            </div>
+        );
+    }
+
 }
-
-export default App;
