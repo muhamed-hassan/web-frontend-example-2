8e979feab722967c2bb0adaf3e7da32e0c772c9c
muhamed-hassan <mohamed.hassan.work@gmail.com>
2024-04-04 14:47:33 +0200

    code refactoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,7 @@ export default class Header extends React.Component {
     render() {
         return (
             <header>
-                <div class="navigation-links">
-                    <Link class="link" to="/">Home</Link>
+                <div class="navigation-links">                    
                     <Link class="link" to="/fragment_1">Fragment_1</Link>
                     <Link class="link" to="/fragment_2">Fragment_2</Link>
                 </div>
